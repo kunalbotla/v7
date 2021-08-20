d919f4260f420d55716475c45dae554490b994c8
Ella <72365100+eilla1@users.noreply.github.com>
2021-08-20 12:59:48 -0700

    Remove default vercel og image

--- pages/_document.js
+++ pages/_document.js
@@ -10,8 +10,6 @@ class MyDocument extends Document {
     const meta = {
       title: 'Kunal Botla',
       description: 'my personal website. kunal@kunalbotla.com.',
-      image:
-        'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
     }
 
     return (
@@ -22,12 +20,9 @@ class MyDocument extends Document {
           <meta property="og:site_name" content={meta.title} />
           <meta property="og:description" content={meta.description} />
           <meta property="og:title" content={meta.title} />
-          <meta property="og:image" content={meta.image} />
-          <meta name="twitter:card" content="summary_large_image" />
           <meta name="twitter:site" content="@kunalbotla" />
           <meta name="twitter:title" content={meta.title} />
           <meta name="twitter:description" content={meta.description} />
-          <meta name="twitter:image" content={meta.image} />
         </Head>
         <body>
           <Main />
