2495f41d1b3a57584bbff1fd5e8ec4e35e6cbb43
Kunal Botla <kunal@kunalbotla.com>
2022-01-23 17:27:40 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,2 +1,16 @@
 const withNextra = require('nextra')('nextra-theme-blog', './theme.config.js')
 module.exports = withNextra()
+
+module.exports = withMDX({
+  pageExtensions: ['ts', 'tsx', 'mdx'],
+  reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: '/zoom',
+        destination: 'https://us02web.zoom.us/j/2536919608?pwd=cG9XeWc1cFI5Z29hZ2x2QVVIbk9KZz09',
+        permanent: false,
+      },
+    ]
+  },
+})
