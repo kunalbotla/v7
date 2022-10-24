328271681b863d7a7bfc6ca18783739778305faf
Kunal Botla <kunal@kunalbotla.com>
2022-10-24 18:55:14 -0400

    `kunalbot.la` redirect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,11 @@ module.exports = withNextra({
  async redirects() {
     return [
       {
+        source: '/',
+        destination: 'https://kunalbot.la',
+        permanent: false,
+      },
+      {
         source: '/zoom',
         destination: 'https://us02web.zoom.us/j/2536919608?pwd=cG9XeWc1cFI5Z29hZ2x2QVVIbk9KZz09',
         permanent: false,
